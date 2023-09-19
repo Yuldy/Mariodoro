a85bb8fb962448179f0d6f1ef0101137a707c0fd
Christian <104484543+Yuldy@users.noreply.github.com>
2023-09-19 13:04:21 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -15,52 +15,111 @@ getButtons.forEach(button => {
 });
 
 
-// Buttons for the rest
-let pokedoroButton = document.getElementById("pokedoro");
+let focusButton = document.getElementById("focus");
 let buttons = document.querySelectorAll(".btn");
 let shortBreakButton = document.getElementById("shortbreak");
-let longBreakButton = document.getElementById('longbreak');
+let longBreakButton = document.getElementById("longbreak");
 let startButton = document.getElementById("btn-start");
 let resetButton = document.getElementById("btn-reset");
 let pause = document.getElementById("btn-pause");
-
 let time = document.getElementById("time");
 let set;
+let active = "focus";
 let count = 59;
 let paused = true;
 let minCount = 24;
-
 time.textContent = `${minCount + 1}:00`;
 
 const appendZero = (value) => {
-    value = value < 10 ? `0${value}` : value;
-    return value;
-}
+  value = value < 10 ? `0${value}` : value;
+  return value;
+};
 
 resetButton.addEventListener(
-    "click",
-    (resetTime = () => {
-        pauseTimer();
-        count = 59;
-        mincount = 24;
-        time.textContent = `${minCount + 1}:00`
-    })
-);
-
-const removePokedoro = () => {
-    buttons.forEach((btn) => {
-        btn.classList.remove("btn-pokedoro")
-    });
-}
-
-pokedoroButton.addEventListener("click", () => {
-    removePokedoro();
-    pokedoroButton.classList.add("btn-focus");
+  "click",
+  (resetTime = () => {
     pauseTimer();
-    minCount = 24;
+    switch (active) {
+      case "long":
+        minCount = 14;
+        break;
+      case "short":
+        minCount = 4;
+        break;
+      default:
+        minCount = 24;
+        break;
+    }
     count = 59;
     time.textContent = `${minCount + 1}:00`;
+  })
+);
+
+const removeFocus = () => {
+  buttons.forEach((btn) => {
+    btn.classList.remove("btn-focus");
+  });
+};
+
+focusButton.addEventListener("click", () => {
+  removeFocus();
+  focusButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 24;
+  count = 59;
+  time.textContent = `${minCount + 1}:00`;
 });
 
-shortBreak
+shortBreakButton.addEventListener("click", () => {
+  active = "short";
+  removeFocus();
+  shortBreakButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 4;
+  count = 59;
+  time.textContent = `${appendZero(minCount + 1)}:00`;
+});
 
+longBreakButton.addEventListener("click", () => {
+  active = "long";
+  removeFocus();
+  longBreakButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 14;
+  count = 59;
+  time.textContent = `${minCount + 1}:00`;
+});
+
+pause.addEventListener(
+  "click",
+  (pauseTimer = () => {
+    paused = true;
+    clearInterval(set);
+    startButton.classList.remove("hide");
+    pause.classList.remove("show");
+    resetButton.classList.remove("show");
+  })
+);
+
+startButton.addEventListener("click", () => {
+  resetButton.classList.add("show");
+  pause.classList.add("show");
+  startButton.classList.add("hide");
+  startButton.classList.remove("show");
+  if (paused) {
+    paused = false;
+    time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+    set = setInterval(() => {
+      count--;
+      time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+      if (count == 0) {
+        if (minCount != 0) {
+          minCount--;
+          count = 60;
+        } else {
+          clearInterval(set);
+        }
+      }
+    }, 1000);
+  }
+});
